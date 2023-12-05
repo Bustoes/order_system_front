56ebe32da2bbfab492f35b0e3121f8a6a5b3f8da
chashao <chashao246972@gmail.com>
2023-12-05 17:11:12 +0800

    FIX:登录时获取身份

--- routes/customer.js
+++ routes/customer.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require("path");
 const router = express.Router();
 
-router.get()
-router.post()
+// router.get()
+// router.post()
 
 module.exports = router;
